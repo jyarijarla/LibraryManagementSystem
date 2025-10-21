44c755e217a4cbcbcc3977674b0f4d0368b77d17
spacecheng <spacecheng04@gmail.com>
2025-10-21 13:43:13 -0500

    added ssl portion

--- server/db.js
+++ server/db.js
@@ -8,6 +8,9 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: true
+  },
   waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
