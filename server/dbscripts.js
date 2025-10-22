6baf92b83c04c51bc170555ecb9a7b78219ff6ab
spacecheng <spacecheng04@gmail.com>
2025-10-21 22:47:01 -0500

    added a cli that allows you to test queries on the database

--- server/dbscripts.js
+++ server/dbscripts.js
@@ -1,4 +1,11 @@
 const db = require("./db");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'db> '
+});
 
 //connection test function
 function testConnection() {
@@ -6,12 +13,36 @@ function testConnection() {
     if (err) {
         console.error('Connection failed:', err.message);
         process.exit(1);
-    } else {
+    }
+    else {
         console.log('Connection successful:', results);
         process.exit(0);
     }
-    })
+    });
 };
+
+//mysql query from node
+function mysqlCLI() {
+    console.log("MySQL CLI. Type 'exit' to quit.");
+    rl.prompt();
+    rl.on('line', (line) => {
+        if(line.trim().toLowerCase() === 'exit') {
+            console.log('Exiting CLI.');
+            rl.close();
+            process.exit(0);
+            return;
+        }
+        db.query(line, (err, results) => {
+            if(err) {
+                console.error('Query error:', err.message);
+            }
+            else {
+                console.log('Query results:', results);
+            }
+            rl.prompt();
+        });
+    });
+}
 
 const command = process.argv[2];//get command name
 const params = process.argv.slice(3);//get command arguments
@@ -24,8 +55,12 @@ switch (command) {
     case 'test-connection':
         testConnection();
         break;
+    case 'mysql-cli':
+        mysqlCLI();
+        break;
 }
-
+//most likely will not be used outside directly running this file
 module.exports = {
-    testConnection
+    testConnection,
+    mysqlCLI
 };
