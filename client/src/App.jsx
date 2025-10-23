b9c2c171593dd53772ad445e7d640b2790b486ce
spacecheng <spacecheng04@gmail.com>
2025-10-22 20:10:06 -0500

    fixed styling issues

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,14 @@ import Login from './pages/Login/Login'
 import Student from './pages/Student/Student'
 import Admin from './pages/Admin/Admin'
 import './App.css'
+import NavBar from './components/Navbar'
 
 function App() {
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/Login" element={<Login />} />
         <Route path="/student" element={<Student />} />
         <Route path="/admin" element={<Admin />} />
       </Routes>
