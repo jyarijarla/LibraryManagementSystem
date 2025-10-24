5f0c1dbff98adcdd4fca8fb967529b1af64dd117
spacecheng <spacecheng04@gmail.com>
2025-10-23 21:59:11 -0500

    Implimented the backend server with some routes

--- server/server.js
+++ server/server.js
@@ -1,1 +1,40 @@
-require("dotenv").config();//imports dotenv that loads environmental variables from /server.env
+require("dotenv").config() ;//imports dotenv that loads environmental variables from /server.env
+const http = require("http");
+const { parse } = require("url");
+
+//Routing map
+const routes = {
+    'POST /api/signup': require('./routes/signup'),
+    'POST /api/login': require('./routes/login')
+}
+
+const server = http.createServer((req, res) => {
+    //CORS handling
+    const allowedOrgins = ['http://localhost:5173'];
+    const origin = req.headers.origin;
+    if (allowedOrgins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.setHeader('Access-Control-Allow-Origin', '');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');//add other methods as needed
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    //parsing the route path requested by the client
+    const routeTarget = new URL(req.url, `http://${req.headers.host}`);
+    const routeKey = `${req.method} ${routeTarget.pathname}`;
+
+    //Fetching the requested route handler based on route map
+    const routeHandler = routes[routeKey];
+    if (routeHandler) {
+        routeHandler(req, res);
+    }
+    else {//handler does not exist or not in route map
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({ message: 'Route not found' }));
+    }
+});
+//Start server
+server.listen(3000, () => {
+    console.log('Server running at http://localhost:3000/');
+})
