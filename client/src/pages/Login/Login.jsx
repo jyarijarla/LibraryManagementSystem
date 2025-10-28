2398570d958fac57d8c9354a90c102351e336f63
jyarijarla24 <jyarijarladev@gmail.com>
2025-10-28 00:41:29 -0500

    changed line 42 to backend link in Login.jsx

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -39,7 +39,7 @@ function Login() {
     setIsLoading(true)
 
     try {
-      const response = await fetch('http://localhost:3000/api/login', {
+      const response = await fetch('https://librarymanagementsystem-z2yw.onrender.com/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
